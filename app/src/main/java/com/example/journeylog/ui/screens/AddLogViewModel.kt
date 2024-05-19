dde30be4a2d6bbfbe2577fba27b8ee78ffaff760
aicccux <72127637+aicccux@users.noreply.github.com>
2024-05-20 07:30:30 +0900

    Add AddLogViewModel

---
+++ app/src/main/java/com/example/journeylog/ui/screens/AddLogViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.journeylog.ui.screens
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.journeylog.JourneyLogApplication
+
+class AddLogViewModel : ViewModel() {
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val savedStateHandle = createSavedStateHandle()
+                val myRepository = (this[APPLICATION_KEY] as JourneyLogApplication)
+                AddLogViewModel(
+
+                )
+            }
+        }
+    }
+}

eaefb31fe1356076dcc71bc8f210b51108fc06b7
aicccux <72127637+aicccux@users.noreply.github.com>
2024-05-20 07:30:16 +0900

    Add HomeViewModel

---
+++ app/src/main/java/com/example/journeylog/ui/screens/HomeViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.journeylog.ui.screens
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.journeylog.JourneyLogApplication
+
+class HomeViewModel : ViewModel() {
+    //    private val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, DB_NAME).build()
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val savedStateHandle = createSavedStateHandle()
+                val myRepository = (this[APPLICATION_KEY] as JourneyLogApplication)
+                HomeViewModel(
+
+                )
+            }
+        }
+    }
+}

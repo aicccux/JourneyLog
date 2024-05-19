8f613ece62199b09ca251b076720307f16ec5197
aicccux <72127637+aicccux@users.noreply.github.com>
2024-05-19 17:59:27 +0900

    Add CameraScreen Compose

---
+++ app/src/main/java/com/example/journeylog/ui/screens/CameraScreen.kt
@@ -0,0 +1,9 @@
+package com.example.journeylog.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun CameraScreen(navController: NavHostController) {
+
+}

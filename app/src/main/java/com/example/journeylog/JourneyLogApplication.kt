9e7a3da1d6f3bc829d9a94b239d6b599b337d966
aicccux <72127637+aicccux@users.noreply.github.com>
2024-05-21 01:11:57 +0900

    Modify JourneyLogApplication

--- app/src/main/java/com/example/journeylog/JourneyLogApplication.kt
+++ app/src/main/java/com/example/journeylog/JourneyLogApplication.kt
@@ -1,10 +1,12 @@
 package com.example.journeylog
 
 import android.app.Application
+import com.example.journeylog.data.PhotoSaverRepository
 
 class JourneyLogApplication : Application() {
+    lateinit var photoSaver: PhotoSaverRepository
     override fun onCreate() {
         super.onCreate()
-
+        photoSaver = PhotoSaverRepository(this, this.contentResolver)
     }
 }

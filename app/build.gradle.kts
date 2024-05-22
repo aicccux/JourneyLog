7e67131941c11f4dc90a9100841275b84c7d418c
aicccux <72127637+aicccux@users.noreply.github.com>
2024-05-23 08:16:08 +0900

    Fix some dependencies problems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.common)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.runtime)
     testImplementation(libs.junit)

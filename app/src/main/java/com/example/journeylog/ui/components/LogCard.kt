e12888c1182c4b05a5067706c19b59fd7af2c027
aicccux <72127637+aicccux@users.noreply.github.com>
2024-05-27 05:39:09 +0900

    Modify view

--- app/src/main/java/com/example/journeylog/ui/components/LogCard.kt
+++ app/src/main/java/com/example/journeylog/ui/components/LogCard.kt
@@ -1,9 +1,7 @@
 package com.example.journeylog.ui.components
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size

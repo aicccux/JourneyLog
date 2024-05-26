1a504abf384e3058712b586f93791462785e0381
aicccux <72127637+aicccux@users.noreply.github.com>
2024-05-27 05:38:19 +0900

    Fix some problems

--- app/src/main/java/com/example/journeylog/ui/screens/AddLogScreen.kt
+++ app/src/main/java/com/example/journeylog/ui/screens/AddLogScreen.kt
@@ -2,6 +2,8 @@ package com.example.journeylog.ui.screens
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
@@ -25,14 +27,12 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -65,15 +65,14 @@ fun AddLogScreen(
     // region State initialization
     val state = viewModel.uiState
     val context = LocalContext.current
-
-    val snackbarHostState = remember { SnackbarHostState() }
+    val snackBarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
     fun canAddPhoto(callback: () -> Unit) {
         if (viewModel.canAddPhoto()) {
             callback()
         } else {
             coroutineScope.launch {
-                snackbarHostState.showSnackbar("You can't add more than $MAX_LOG_PHOTOS_LIMIT photos")
+                snackBarHostState.showSnackbar("You can't add more than $MAX_LOG_PHOTOS_LIMIT photos")
             }
         }
     }
@@ -88,7 +87,7 @@ fun AddLogScreen(
                 }
             } else {
                 coroutineScope.launch {
-                    snackbarHostState.showSnackbar("Camera currently disabled due to denied permission.")
+                    snackBarHostState.showSnackbar("Camera currently disabled due to denied permission.")
                 }
             }
         }
@@ -111,20 +110,17 @@ fun AddLogScreen(
                 viewModel.fetchLocation()
             } else {
                 coroutineScope.launch {
-                    snackbarHostState.showSnackbar("Location currently disabled due to denied permission.")
+                    snackBarHostState.showSnackbar("Location currently disabled due to denied permission.")
                 }
             }
         }
 
     var showExplanationDialogForLocationPermission by remember { mutableStateOf(false) }
     if (showExplanationDialogForLocationPermission) {
-        LocationExplanationDialog(
-            onConfirm = {
-                requestLocationPermissions.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-                showExplanationDialogForLocationPermission = false
-            },
-            onDismiss = { showExplanationDialogForLocationPermission = false }
-        )
+        LocationExplanationDialog(onConfirm = {
+            requestLocationPermissions.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+            showExplanationDialogForLocationPermission = false
+        }, onDismiss = { showExplanationDialogForLocationPermission = false })
     }
     // region helper functions
 
@@ -152,45 +148,36 @@ fun AddLogScreen(
             callback()
         } else {
             coroutineScope.launch {
-                snackbarHostState.showSnackbar("You haven't completed all details")
+                snackBarHostState.showSnackbar("You haven't completed all details")
             }
         }
     }
     // endregion
-    Scaffold(
-        snackbarHost = { SnackbarHost(snackbarHostState) },
-        topBar = {
-            TopAppBar(title = { Text("Add Log", fontFamily = FontFamily.Serif) },
-                navigationIcon = {
-                    if (navController.previousBackStackEntry != null) {
-                        IconButton(onClick = { navController.navigateUp() }) {
-                            Icon(
-                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                contentDescription = "Back"
-                            )
-                        }
-                    }
+
+    Scaffold(snackbarHost = { SnackbarHost(snackBarHostState) }, topBar = {
+        TopAppBar(title = { Text("Add Log", fontFamily = FontFamily.Serif) }, navigationIcon = {
+            if (navController.previousBackStackEntry != null) {
+                IconButton(onClick = { navController.navigateUp() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
                 }
-            )
-        },
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text("Save log") },
-                icon = {
-                    if (state.isSaving) {
-                        CircularProgressIndicator(Modifier.size(24.0.dp))
-                    } else {
-                        Icon(Icons.Filled.Check, null)
-                    }
-                },
-                onClick = {
-                    canSaveLog {
-                        viewModel.createLog()
-                    }
-                }
-            )
-        }
-    ) { innerPadding ->
+            }
+        })
+    }, floatingActionButton = {
+        ExtendedFloatingActionButton(text = { Text("Save log") }, icon = {
+            if (state.isSaving) {
+                CircularProgressIndicator(Modifier.size(24.0.dp))
+            } else {
+                Icon(Icons.Filled.Check, null)
+            }
+        }, onClick = {
+            canSaveLog {
+                viewModel.createLog()
+            }
+        })
+    }) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
@@ -198,91 +185,86 @@ fun AddLogScreen(
             verticalArrangement = Arrangement.Center
         ) {
             // region Date
-            ListItem(
-                headlineContent = { Text("Date") },
-                trailingContent = {
-                    DatePickerCompose(
-                        state.date,
-                        onChange = viewModel::onDateChange
-                    )
-                }
-            )
+            ListItem(headlineContent = { Text("Date") }, trailingContent = {
+                DatePickerCompose(
+                    state.date, onChange = viewModel::onDateChange
+                )
+            })
             HorizontalDivider()
             // endregion
 
             // region Location
-            ListItem(
-                headlineContent = { Text("Location") },
-                trailingContent = {
-                    LocationPicker(state.place) {
-                        when {
-                            state.hasLocationAccess -> viewModel.fetchLocation()
-                            ActivityCompat.shouldShowRequestPermissionRationale(
-                                context.applicationContext as Activity,
-                                Manifest.permission.ACCESS_COARSE_LOCATION
-                            ) ->
-                                showExplanationDialogForLocationPermission = true
+            ListItem(headlineContent = { Text("Location") }, trailingContent = {
+                LocationPicker(state.place) {
+                    when {
+                        state.hasLocationAccess -> viewModel.fetchLocation()
+                        ActivityCompat.shouldShowRequestPermissionRationale(
+                            context.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) -> showExplanationDialogForLocationPermission = true
 
-                            else -> requestLocationPermissions.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-                        }
+                        else -> requestLocationPermissions.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
                     }
                 }
-            )
+            })
             HorizontalDivider()
             // endregion
 
             // region Photos
-            ListItem(
-                headlineContent = { Text("Photos") },
-                trailingContent = {
-                    Row {
-                        // region Photo Picker
-                        TextButton(onClick = {
-                            canAddPhoto {
-                                viewModel.loadLocalPickerPictures()
-                                coroutineScope.launch {
-                                    pickImage.launch(
-                                        PickVisualMediaRequest(
-                                            ActivityResultContracts.PickVisualMedia.ImageOnly
-                                        )
+            ListItem(headlineContent = { Text("Photos") }, trailingContent = {
+                Row {
+                    // region Photo Picker
+                    TextButton(onClick = {
+                        canAddPhoto {
+                            viewModel.loadLocalPickerPictures()
+                            coroutineScope.launch {
+                                pickImage.launch(
+                                    PickVisualMediaRequest(
+                                        ActivityResultContracts.PickVisualMedia.ImageOnly
                                     )
-                                }
+                                )
                             }
-                        }) {
-                            Icon(Icons.Filled.ShoppingCart, null)
-                            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                            Text("Add photo")
                         }
-                        // endregion
-
-                        // region Camera
-                        IconButton(onClick = {
-                            canAddPhoto {
-                                when {
-                                    state.hasCameraAccess -> navController.navigate(Screens.Camera.route)
-                                    ActivityCompat.shouldShowRequestPermissionRationale(
-                                        context.applicationContext as Activity,
-                                        Manifest.permission.CAMERA
-                                    ) -> showExplanationDialogForCameraPermission = true
-
-                                    else -> requestCameraPermission.launch(Manifest.permission.CAMERA)
-                                }
-                            }
-                        })
-                        {
-                            Icon(Icons.Filled.Add, null)
-                        }
-                        // endregion
+                    }) {
+                        Icon(Icons.Filled.ShoppingCart, null)
+                        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                        Text("Add photo")
                     }
+                    // endregion
+
+                    // region Camera
+                    IconButton(onClick = {
+                        canAddPhoto {
+                            when {
+                                state.hasCameraAccess -> navController.navigate(Screens.Camera.route)
+                                ActivityCompat.shouldShowRequestPermissionRationale(
+                                    context.getActivity(), Manifest.permission.CAMERA
+                                ) -> showExplanationDialogForCameraPermission = true
+
+                                else -> requestCameraPermission.launch(Manifest.permission.CAMERA)
+                            }
+                        }
+                    }) {
+                        Icon(Icons.Filled.Add, null)
+                    }
+                    // endregion
                 }
-            )
+            })
             // endregion
 
-            PhotoGrid(
-                modifier = Modifier.padding(16.dp),
+            PhotoGrid(modifier = Modifier.padding(16.dp),
                 photos = state.savedPhotos,
-                onRemove = { photo -> viewModel.onPhotoRemoved(photo) }
-            )
+                onRemove = { photo -> viewModel.onPhotoRemoved(photo) })
         }
     }
+}
+
+fun Context.getActivity(): Activity {
+    var currentContext = this
+    while (currentContext is ContextWrapper) {
+        if (currentContext is Activity) {
+            return currentContext
+        }
+        currentContext = currentContext.baseContext
+    }
+    throw IllegalStateException("Permissions should be called in the context of an Activity")
 }

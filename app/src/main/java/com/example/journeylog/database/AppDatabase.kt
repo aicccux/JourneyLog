9686c4659b63390cef6aa4d1817739a0d56cf0e2
aicccux <72127637+aicccux@users.noreply.github.com>
2024-05-27 04:16:56 +0900

    Delete useless code

--- app/src/main/java/com/example/journeylog/database/AppDatabase.kt
+++ app/src/main/java/com/example/journeylog/database/AppDatabase.kt
@@ -8,21 +8,5 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun logDao(): LogDao
     companion object {
         const val DB_NAME = "log_database"
-//        @Volatile
-//        private var INSTANCE: AppDatabase? = null
-//        fun getDatabase(context: Context): AppDatabase {
-//            // if the INSTANCE is not null, then return it,
-//            // if it is, then create the database
-//            return INSTANCE ?: synchronized(this) {
-//                val instance = Room.databaseBuilder(
-//                    context.applicationContext,
-//                    AppDatabase::class.java,
-//                    DB_NAME
-//                ).build()
-//                INSTANCE = instance
-//                // return instance
-//                instance
-//            }
-//        }
     }
 }

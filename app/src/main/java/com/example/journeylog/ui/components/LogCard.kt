d2b0a9075064c2555914c3f4942f2a6a494091c9
aicccux <72127637+aicccux@users.noreply.github.com>
2024-06-15 21:00:22 +0900

    Update LogCard.kt

--- app/src/main/java/com/example/journeylog/ui/components/LogCard.kt
+++ app/src/main/java/com/example/journeylog/ui/components/LogCard.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -15,6 +15,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -25,15 +29,19 @@ fun LogCard(modifier: Modifier, log: Log, formattedDate: String, onDelete: (log:
     Card(
         modifier = Modifier.padding(horizontal = 8.dp)
     ) {
+        var isDeleteLog by remember { mutableStateOf(false) }
+        if (isDeleteLog) {
+            DeleteConfirmDialog(onDismiss = { isDeleteLog = false }, log = log, onDelete = onDelete)
+        }
         Row(Modifier.padding(8.dp, 0.dp), verticalAlignment = Alignment.CenterVertically) {
             Text(
                 text = formattedDate,
                 modifier = Modifier.weight(1f),
                 style = MaterialTheme.typography.headlineSmall
             )
-            IconButton(onClick = { onDelete(log) }) {
+            IconButton(onClick = { isDeleteLog = true }) {
                 Icon(
-                    imageVector = Icons.Filled.Delete, contentDescription = "Delete log"
+                    imageVector = Icons.Filled.Close, contentDescription = "Delete log"
                 )
             }
         }
